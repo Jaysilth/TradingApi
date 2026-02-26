20b2619a40cc0698d26b50440145aa9321a35017
Onadipe JJ <onadipejohn@gmail.com>
2026-02-26 09:44:15 +0100

    Intial commit - basic project structure with test controller

---
+++ TradingApi/src/main/java/com/journal/TradingApi/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.journal.TradingApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Trading api is active";
+    }
+
+}
